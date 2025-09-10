b06a7471ec643afd771d94ac0b16f5dc6a329a23
Jan-Eric-B <Jan-Eric.B@pm.me>
2025-09-10 17:19:53 +0200

    Added Shared AppStateService to use values from other viewmodels

---
+++ src/DeDupe/Services/AppStateService.cs
@@ -0,0 +1,112 @@
+﻿using DeDupe.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using Windows.Storage;
+
+namespace DeDupe.Services
+{
+    /// <summary>
+    /// Centralized state management service for the application.
+    /// </summary>
+    public partial class AppStateService : IAppStateService
+    {
+        #region Fields
+
+        private readonly List<string> _filePaths = [];
+
+        private readonly List<ProcessedMedia> _processedImages = [];
+
+        private string _tempFolderPath = ApplicationData.Current.TemporaryFolder.Path + Path.DirectorySeparatorChar + "ProcessedImages";
+
+        #endregion Fields
+
+        #region Properties
+
+        public IReadOnlyCollection<string> FilePaths => _filePaths.AsReadOnly();
+
+        public int FileCount => _filePaths.Count;
+
+        public string TempFolderPath
+        {
+            get => _tempFolderPath;
+            set
+            {
+                if (_tempFolderPath != value)
+                {
+                    _tempFolderPath = value;
+                    OnPropertyChanged();
+                    TempFolderPathChanged?.Invoke(this, EventArgs.Empty);
+                }
+            }
+        }
+
+        public IReadOnlyCollection<ProcessedMedia> ProcessedImages => _processedImages.AsReadOnly();
+
+        public int ProcessedImageCount => _processedImages.Count;
+
+        #endregion Properties
+
+        #region Methods
+
+        public void SetFilePaths(IEnumerable<string> filePaths)
+        {
+            _filePaths.Clear();
+            _filePaths.AddRange(filePaths ?? []);
+
+            OnPropertyChanged(nameof(FilePaths));
+            OnPropertyChanged(nameof(FileCount));
+            FilePathsChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void SetProcessedImages(IEnumerable<ProcessedMedia> processedImages)
+        {
+            _processedImages.Clear();
+            _processedImages.AddRange(processedImages ?? []);
+
+            OnPropertyChanged(nameof(ProcessedImages));
+            OnPropertyChanged(nameof(ProcessedImageCount));
+            ProcessedImagesChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void AddProcessedImage(ProcessedMedia processedImage)
+        {
+            if (processedImage != null)
+            {
+                _processedImages.Add(processedImage);
+                OnPropertyChanged(nameof(ProcessedImages));
+                OnPropertyChanged(nameof(ProcessedImageCount));
+                ProcessedImagesChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public void ClearProcessedImages()
+        {
+            _processedImages.Clear();
+            OnPropertyChanged(nameof(ProcessedImages));
+            OnPropertyChanged(nameof(ProcessedImageCount));
+            ProcessedImagesChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        #endregion Methods
+
+        #region Events
+
+        public event EventHandler? FilePathsChanged;
+
+        public event EventHandler? ProcessedImagesChanged;
+
+        public event EventHandler? TempFolderPathChanged;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion Events
+    }
+}
---
+++ src/DeDupe/Services/IAppStateService.cs
@@ -0,0 +1,85 @@
+﻿using DeDupe.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace DeDupe.Services
+{
+    /// <summary>
+    /// Centralized state management service for the application.
+    /// </summary>
+    public interface IAppStateService : INotifyPropertyChanged
+    {
+        #region FileInput Data
+
+        /// <summary>
+        /// File paths
+        /// </summary>
+        IReadOnlyCollection<string> FilePaths { get; }
+
+        /// <summary>
+        /// Update file paths
+        /// </summary>
+        /// <param name="filePaths">New collection of file paths</param>
+        void SetFilePaths(IEnumerable<string> filePaths);
+
+        /// <summary>
+        /// Count of files
+        /// </summary>
+        int FileCount { get; }
+
+        #endregion FileInput Data
+
+        #region PreProcessing Data
+
+        string TempFolderPath { get; set; }
+
+        /// <summary>
+        /// Processed files
+        /// </summary>
+        IReadOnlyCollection<ProcessedMedia> ProcessedImages { get; }
+
+        /// <summary>
+        /// Update processed files
+        /// </summary>
+        /// <param name="processedImages">New collection of processed images</param>
+        void SetProcessedImages(IEnumerable<ProcessedMedia> processedImages);
+
+        /// <summary>
+        /// Add processed file
+        /// </summary>
+        /// <param name="processedImage">Adds processed image</param>
+        void AddProcessedImage(ProcessedMedia processedImage);
+
+        /// <summary>
+        /// Clear processed images
+        /// </summary>
+        void ClearProcessedImages();
+
+        /// <summary>
+        /// Count of processed images
+        /// </summary>
+        int ProcessedImageCount { get; }
+
+        #endregion PreProcessing Data
+
+        #region Events
+
+        /// <summary>
+        /// File paths collection changes
+        /// </summary>
+        event EventHandler? FilePathsChanged;
+
+        /// <summary>
+        /// Processed images collection changes
+        /// </summary>
+        event EventHandler? ProcessedImagesChanged;
+
+        /// <summary>
+        /// Temp folder changes
+        /// </summary>
+        event EventHandler? TempFolderPathChanged;
+
+        #endregion Events
+    }
+}
