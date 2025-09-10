e323919171672cf6bbc742a980c7d106cd143488
Jan-Eric-B <Jan-Eric.B@pm.me>
2025-09-10 17:18:19 +0200

    More cleaning up

--- src/DeDupe/Services/NavigationService.cs
+++ src/DeDupe/Services/NavigationService.cs
@@ -6,10 +6,16 @@ namespace DeDupe.Services
 {
     internal class NavigationService(IServiceProvider serviceProvider) : INavigationService
     {
+        #region Properties
+
         private readonly IServiceProvider _serviceProvider = serviceProvider;
 
         private const int StepIndexMin = 0;
         private const int StepIndexMax = 3;
+
+        #endregion Properties
+
+        #region Methods
 
         public void NavigateToStep(int stepIndex)
         {
@@ -18,5 +24,7 @@ namespace DeDupe.Services
             MainWindowViewModel? mainViewModel = _serviceProvider.GetService<MainWindowViewModel>();
             mainViewModel?.NavigateToTabCommand.Execute(clampedIndex);
         }
+
+        #endregion Methods
     }
 }
