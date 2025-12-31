e1af6ccba2abed4b8adafb28d10da39db5ce74d6
Jan-Eric-B <Jan-Eric.B@pm.me>
2025-12-31 14:10:39 +0100

    Integrated enum Description Data Annotation with Converter and Helper method

--- src/DeDupe/Enums/InterpolationMethod.cs
+++ src/DeDupe/Enums/InterpolationMethod.cs
@@ -1,11 +1,22 @@
-﻿namespace DeDupe.Enums
+﻿using System.ComponentModel;
+
+namespace DeDupe.Enums
 {
     public enum InterpolationMethod
     {
+        [Description("Nearest Neighbor")]
         NearestNeighbor,
+
+        [Description("Bilinear")]
         Bilinear,
+
+        [Description("Bicubic")]
         Bicubic,
+
+        [Description("Fant")]
         Fant,
+
+        [Description("Lanczos")]
         Lanczos
     }
 }
