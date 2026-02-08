38e0b0d81ca6d6681ecfbdc52b8ea6cf85162c40
Jan-Eric-B <Jan-Eric.B@pm.me>
2026-02-08 17:43:58 +0100

    Added support for tif file and changed out string[] with FrozenSet<>

--- src/DeDupe/Constants/SupportedFileExtensions.cs
+++ src/DeDupe/Constants/SupportedFileExtensions.cs
@@ -1,44 +1,46 @@
 ﻿using System;
+using System.Collections.Frozen;
+using System.Linq;
 
 namespace DeDupe.Constants
 {
     public static class SupportedFileExtensions
     {
-        public static readonly string[] SupportedImageExtensions =
-        [
-            ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".webp"
-        ];
+        public static readonly FrozenSet<string> SupportedImageExtensions = FrozenSet.ToFrozenSet(
+            [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".tif", ".webp"],
+            StringComparer.OrdinalIgnoreCase
+        );
 
-        public static readonly string[] SupportedVideoExtensions =
-        [
-            ".mp4", ".avi", ".mov", ".wmv", ".flv", ".webm", ".mkv", ".m4v"
-        ];
+        public static readonly FrozenSet<string> SupportedVideoExtensions = FrozenSet.ToFrozenSet(
+            [".mp4", ".avi", ".mov", ".wmv", ".flv", ".webm", ".mkv", ".m4v"],
+            StringComparer.OrdinalIgnoreCase
+        );
 
-        public static readonly string[] SupportedModelExtensions =
-        [
-            ".onnx"
-        ];
+        public static readonly FrozenSet<string> SupportedModelExtensions = FrozenSet.ToFrozenSet(
+            [".onnx"],
+            StringComparer.OrdinalIgnoreCase
+        );
 
-        public static readonly string[] AllSupportedExtensions = [.. SupportedImageExtensions, .. SupportedVideoExtensions];
+        public static readonly FrozenSet<string> AllSupportedExtensions = FrozenSet.ToFrozenSet(SupportedImageExtensions.Concat(SupportedVideoExtensions), StringComparer.OrdinalIgnoreCase);
 
         public static bool IsImageFile(string extension)
         {
-            return SupportedImageExtensions.Contains(extension.ToLowerInvariant());
+            return SupportedImageExtensions.Contains(extension);
         }
 
         public static bool IsVideoFile(string extension)
         {
-            return SupportedVideoExtensions.Contains(extension.ToLowerInvariant());
+            return SupportedVideoExtensions.Contains(extension);
         }
 
         public static bool IsSupportedMediaFile(string extension)
         {
-            return AllSupportedExtensions.Contains(extension.ToLowerInvariant());
+            return AllSupportedExtensions.Contains(extension);
         }
 
         public static bool IsSupportedModelFile(string extension)
         {
-            return SupportedModelExtensions.Contains(extension.ToLowerInvariant());
+            return SupportedModelExtensions.Contains(extension);
         }
     }
 }
