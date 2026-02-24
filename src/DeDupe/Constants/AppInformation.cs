0aaa112d972bf2ee29f3e14132d13d611d843091
Jan-Eric-B <Jan-Eric.B@pm.me>
2026-02-24 07:52:47 +0100

    Updated packages and added Privacy Policy link

--- src/DeDupe/Constants/AppInformation.cs
+++ src/DeDupe/Constants/AppInformation.cs
@@ -8,6 +8,7 @@ namespace DeDupe.Constants
         public const string ModelReleaseTag = "v1.0.0-models";
         public const string GitHubRepo = $"https://github.com/Jan-Eric-B/{AppName}";
         public const string License = $"{GitHubRepo}/blob/main/LICENSE";
+        public const string Privacy = $"{GitHubRepo}/blob/main/PRIVACY.md";
 
         public static readonly string Version = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "0.0";
 
